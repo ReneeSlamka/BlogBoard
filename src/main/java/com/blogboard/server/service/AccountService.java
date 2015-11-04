b49f50b5b646b8887d2f06116cc541dab44150b7
Renée <renee.slamka@gmail.com>
2015-11-04 15:00:01 -0800

    Replaced hardcoded error strings with constants and added hashing to passwords when creating accounts and logging in

--- src/main/java/com/blogboard/server/service/AccountService.java
+++ src/main/java/com/blogboard/server/service/AccountService.java
@@ -1,5 +1,7 @@
 package com.blogboard.server.service;
 
+import com.blogboard.server.data.entity.Session;
+import com.blogboard.server.data.repository.SessionRepository;
 import com.blogboard.server.web.CreateAccountResponse;
 import com.blogboard.server.web.LoginResponse;
 import com.blogboard.server.web.ValidateUserSessionResponse;
@@ -7,13 +9,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.blogboard.server.data.entity.Account;
 import com.blogboard.server.data.repository.AccountRepository;
+import java.security.MessageDigest;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.Random;
 
 @Service
 public class AccountService {
 
     private AccountRepository accountRepo;
+    private SessionRepository sessionRepo;
+
+    public static final String UNKNOWN_DATABASE_ERROR = "Uknown database error";
+    public static final String INVALID_LOGIN_ATTEMPT = "Session already in place, invalid login atttempt";
+    public static final String INVALID_SESSION = "Not a valid session";
+    public static final String NO_SESSION_FOUND = "No session has been initialized";
+    public static final String INVALID_USERNAME = "username";
+    public static final String INVALID_PASSWORD = "password";
+    public static final String INVALID_EMAIL = "email";
+    public static final String UNKNOWN_ERROR = "Unknown error";
 
     //TODO: how to ensure only one return value for queries that require it?
 
@@ -22,26 +36,40 @@ public class AccountService {
         this.accountRepo = accountRepository;
     }
 
+    @Autowired
+    public void setSessionRepository(SessionRepository sessionRepository) {
+        this.sessionRepo = sessionRepository;
+    }
+
 
     public CreateAccountResponse createAccount(String username, String password, String email) {
         CreateAccountResponse createAccountResponse = new CreateAccountResponse();
 
         if (accountRepo.findByUsername(username) == null && accountRepo.findByEmail(email) == null) {
-            Account newAccount = new Account(username, password, email);
+
+            String hashedPassword = password;
+            try {
+                hashedPassword = hashPassword(password);
+            } catch (NoSuchAlgorithmException e) {
+                System.out.println("Hashing function failed to hash password");
+            }
+
+            Account newAccount = new Account(username, hashedPassword, email);
             Account savedAccount = accountRepo.save(newAccount);
+
             if (savedAccount == null) {
-                createAccountResponse.setToFailure("Uknown database error");
+                createAccountResponse.setToFailure(UNKNOWN_DATABASE_ERROR);
             } else {
                 createAccountResponse.setToSuccess();
             }
         } else {
             if(accountRepo.findByUsername(username) != null) {
-                createAccountResponse.setToFailure("username");
+                createAccountResponse.setToFailure(INVALID_USERNAME);
             } else if (accountRepo.findByEmail(email) != null) {
-                createAccountResponse.setToFailure("email");
+                createAccountResponse.setToFailure(INVALID_EMAIL);
             } else {
                 //to cover other unknown errors
-                createAccountResponse.setToFailure("UNKNOWN ERROR"); //TODO replace later
+                createAccountResponse.setToFailure(UNKNOWN_DATABASE_ERROR); //TODO replace later
             }
         }
 
@@ -52,25 +80,34 @@ public class AccountService {
         LoginResponse loginResponse = new LoginResponse();
         Account targetAccount = accountRepo.findByUsername(username);
 
-        if (targetAccount != null && targetAccount.getPassword().equals(password)){
-            //generate session id and store it to this account in db
-            String newSessionId;
-            //ensure new session id is different from the previous one
-            do {
-                newSessionId = generateSessionID();
-            } while(generateSessionID().equals(targetAccount.getSessionId()));
+        String hashedPassword = password;
+        try {
+            hashedPassword = hashPassword(password);
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("Hashing function failed to hash password");
+        }
 
-            targetAccount.setSessionId(newSessionId);
+        if (targetAccount != null && targetAccount.getPassword().equals(hashedPassword)){
+            //generate session id and store it to this account in db
+            Session previousSession = sessionRepo.findByAccountUsername(username);
+            if (previousSession == null) {
+                String newSessionId = generateSessionID();
+                Session newSession = new Session(username, newSessionId);
+                loginResponse.setToSuccess(newSessionId);
+            } else {
+                loginResponse.setToFailure(INVALID_LOGIN_ATTEMPT);
+            }
+
             Account updatedAccount = accountRepo.save(targetAccount);
-            loginResponse.setToSuccess(newSessionId);
+            loginResponse.setToSuccess("");//newSessionId);
         }
         else {
             if (accountRepo.findByUsername(username) == null) {
-                loginResponse.setToFailure("username");
+                loginResponse.setToFailure(INVALID_USERNAME);
             } else if (!accountRepo.findByUsername(username).getPassword().equals(password)) {
-                loginResponse.setToFailure("password");
+                loginResponse.setToFailure(INVALID_PASSWORD);
             } else {
-                loginResponse.setToFailure("UNKNOWN ERROR"); //TODO replace with proper solution later
+                loginResponse.setToFailure(UNKNOWN_ERROR); //TODO replace with proper solution later
             }
         }
 
@@ -81,17 +118,17 @@ public class AccountService {
         ValidateUserSessionResponse validationResponse = new ValidateUserSessionResponse();
 
         if (sessionId.length() == 0){
-            validationResponse.setToFailure("No session has been initialized");
+            validationResponse.setToFailure(NO_SESSION_FOUND);
             return validationResponse;
         }
 
-        Account targetAccount = accountRepo.findBySessionId(sessionId);
+        Session targetAccount = sessionRepo.findBySessionId(sessionId);
 
         //TODO: improve security here after more research
         if (targetAccount != null){
             validationResponse.setToSuccess();
         } else {
-            validationResponse.setToFailure("Not a valid session");
+            validationResponse.setToFailure(INVALID_SESSION);
         }
 
         return validationResponse;
@@ -113,6 +150,21 @@ public class AccountService {
         String sessionId = "ABC" + String.valueOf(randomInt);
 
         return sessionId;
+    }
+
+    private String hashPassword(String password) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(password.getBytes());
+
+        byte byteData[] = md.digest();
+
+        //convert the byte to hex format method 1
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < byteData.length; i++) {
+            sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        return sb.toString();
     }
 
 }
