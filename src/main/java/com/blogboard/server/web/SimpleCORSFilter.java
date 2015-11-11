d3855318d77533aaeb701abda7982733bd3ad367
Renée <renee.slamka@gmail.com>
2015-11-10 17:28:22 -0800

    Updated CORS filter

--- src/main/java/com/blogboard/server/web/SimpleCORSFilter.java
+++ src/main/java/com/blogboard/server/web/SimpleCORSFilter.java
@@ -13,14 +13,13 @@ import org.springframework.stereotype.Component;
 public class SimpleCORSFilter implements Filter{
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        response.setHeader("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,X-Prototype-Version,Cache-Control,Pragma");
         response.setHeader("Custom-Header", "Own-Data");
-        response.setHeader("Access-Control-Expose-Headers", "Custom-Header");
-        response.setHeader("Access-Control-Expose-Headers", "Location, Set-Cookie, SessionId");
+        response.setHeader("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Location, Custom-Header, Set-Cookie, SessionId");
         chain.doFilter(req, res);
     }
 
