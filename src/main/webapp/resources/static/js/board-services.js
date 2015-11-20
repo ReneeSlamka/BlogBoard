aaada9a7e7f25a0373b5d6c406b2e318e445a90f
Renée <renee.slamka@gmail.com>
2015-11-19 18:04:45 -0800

    Temporary test changes to js board services

--- src/main/webapp/resources/static/js/board-services.js
+++ src/main/webapp/resources/static/js/board-services.js
@@ -1,6 +1,7 @@
 
 function createBoard() {
-    var $newBoardname = $("#new-board-name").val();
+    var test = $("#new-board-name").val();
+    var $newBoardname = encodeURI($("#new-board-name").val());
 
     $.ajax({
         type: 'POST',
@@ -19,25 +20,15 @@ function createBoard() {
             var url = request.getResponseHeader("Location");
             if(textStatus === "success") {
                 $("#create-board-form").modal("hide");
-                var boardName = getCookie("userBoards");
-                //remove extra quotation marks
-                boardName = boardName.substring(1, boardName.length-1);
-                boardName = boardName.split(/[\[\]']+/g);
-                boardName = boardName[1].replace(/\\/g, "");
-                
-                var arrayCookieJson = boardName.split(",");
-
-                for (var i = 0; i < arrayCookieJson.length; i=i+2) {
-                    arrayCookieJson[i/2] = arrayCookieJson[i] + "," + arrayCookieJson[i+1];
-                    var cookieJSON = JSON.parse(arrayCookieJson[i/2]);
-                    addBoardName(cookieJSON.name, cookieJSON.url);
-                }
+                //var boardName = getCookie("userBoards");
+                //parseJSONBoardCookies(boardName)
+                var newBoard = request.responseJSON.board;
+                addBoardName(newBoard.name, newBoard.url);
             }
             document.getElementById("new-board-name").value = "";
             if (window.location.href !== url) {
                 window.location.href = url;
             }
-
         }
     });
 }
@@ -45,4 +36,20 @@ function createBoard() {
 function addBoardName(newBoardName, url) {
     var $listBoards = $("#user-boards-list");
     $listBoards.append("<a href=" + url + "><li>" + newBoardName+ "</li></a>");
+}
+
+//function to parse board cookie from server to list names on home page
+function parseJSONBoardCookies(jsonArray) {
+    //remove extra quotation marks
+    jsonArray = jsonArray.substring(1, boardName.length-1);
+    jsonArray = jsonArray.split(/[\[\]']+/g);
+    jsonArray = jsonArray[1].replace(/\\/g, "");
+
+    var arrayCookieJson = jsonArray.split(",");
+
+    for (var i = 0; i < arrayCookieJson.length; i=i+2) {
+        arrayCookieJson[i/2] = arrayCookieJson[i] + "," + arrayCookieJson[i+1];
+        //var cookieJSON = JSON.parse(arrayCookieJson[i/2]);
+        //addBoardName(cookieJSON.name, cookieJSON.url);
+    }
 }
