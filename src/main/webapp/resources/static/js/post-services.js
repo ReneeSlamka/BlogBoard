a817166415a4f4130eaf5eb8ccf3868d963888b8
Renée <renee.slamka@gmail.com>
2015-12-04 13:41:02 -0800

    Refactored post element ids to use use object id numbers only, no names as can contain spaces. Also fixed up the edit, save and delete methods, all now working

--- src/main/webapp/resources/static/js/post-services.js
+++ src/main/webapp/resources/static/js/post-services.js
@@ -1,15 +1,15 @@
 
-function savePostChanges(postId, postTitleInputId, postTextContentInputId) {
+function savePostChanges(postId, titleId, textContentId) {
     var apiUrl = window.location.href + '/posts/' + postId;
-    var $title = $("#" + postTitleInputId).val().trim();
-    var $textBody = $("#" + postTextContentInputId).val().trim();
+    var title = document.getElementById("edit-" + titleId).value;
+    var textBody = document.getElementById("edit-" + textContentId).value;
 
     $.ajax({
-        type: 'PUT',
+        type: 'POST',
         cache: false,
         async: false,
         url: apiUrl,
-        data: {"title": $title, "textBody": $textBody},
+        data: {"title": title, "textBody": textBody},
         dataType: 'json',
         crossDomain: true,
         error: function (request, textStatus, errorThrown) {
@@ -19,22 +19,38 @@ function savePostChanges(postId, postTitleInputId, postTextContentInputId) {
             alert(request.responseText);
             //Todo: temporary solution for user friendly error
             if(request.responseJSON.error === undefined) {
-
+                document.getElementById("edit-" + titleId).style.display = "none";
+                document.getElementById("edit-" + textContentId).style.display = "none";
+                document.getElementById(titleId).style.display = "block";
+                document.getElementById(titleId).innerHTML = title;
+                document.getElementById(textContentId).style.display = "block";
+                document.getElementById(textContentId).innerHTML = textBody;
             }
         }
     });
 }
 
-function showEditingFields(titleEditingInputId, textContentEditingInputId) {
-    document.getElementById(titleEditingInputId).style.display = "block";
-    document.getElementById(textContentEditingInputId).style.display = "block";
+function showEditingFields(titleId, textContentId) {
+    var title = document.getElementById(titleId).innerHTML;
+    var textBody =  document.getElementById(textContentId).innerHTML;
+
+    var inputTitleElement =  document.getElementById("edit-" + titleId);
+    inputTitleElement.style.display = "block";
+    inputTitleElement.value = title;
+
+    var inputTextContent = document.getElementById("edit-" + textContentId);
+    inputTextContent.style.display = "block";
+    inputTextContent.innerHTML = textBody;
+
+    document.getElementById(titleId).style.display = "none";
+    document.getElementById(textContentId).style.display = "none";
 }
 
 function deletePost(postId, postElementId) {
     var apiUrl = window.location.href + '/posts/' + postId;
 
     $.ajax({
-        type: 'POST',
+        type: 'DELETE',
         cache: false,
         async: false,
         url: apiUrl,
